f5c3e44dcc7ce4d3127e1d83177b6f5f707dced0
Vlad <vladpobidash@gmail.com>
2021-11-16 20:23:36 +0200

    added login form & textField

---
+++ src/utils/validator.js
@@ -0,0 +1,49 @@
+/* eslint-disable indent */
+export function validator(data, config) {
+  const errors = {};
+
+  function validate(validateMethod, data, config) {
+    let statusValidate;
+    const regExps = {
+      email: /^\S+@\S+\.\S+$/g,
+      capital: /[A-Z]+/g,
+      digit: /\d+/g
+    };
+
+    switch (validateMethod) {
+      case "isRequired":
+        statusValidate = data.trim() === "";
+        break;
+      case "isEmail":
+        statusValidate = !regExps.email.test(data);
+        break;
+      case "isCapitalSymbol":
+        statusValidate = !regExps.capital.test(data);
+        break;
+      case "isContainDigit":
+        statusValidate = !regExps.digit.test(data);
+        break;
+      case "min":
+        statusValidate = data.length < config.value;
+        break;
+    }
+
+    if (statusValidate) return config.message;
+  }
+
+  for (const fildName in data) {
+    for (const validateMethod in config[fildName]) {
+      const error = validate(
+        validateMethod,
+        data[fildName],
+        config[fildName][validateMethod]
+      );
+
+      if (error && !errors[fildName]) {
+        errors[fildName] = error;
+      }
+    }
+  }
+
+  return errors;
+}
